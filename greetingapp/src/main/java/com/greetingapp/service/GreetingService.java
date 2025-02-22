e7afb8873c84ed01ee74f8a33423e6490faaefce
redbyte09 <sakshisharma4234@gmail.com>
2025-02-22 23:01:29 +0530

    [Sakshi] create app to give greeting message with user first and last name

--- greetingapp/src/main/java/com/greetingapp/service/GreetingService.java
+++ greetingapp/src/main/java/com/greetingapp/service/GreetingService.java
@@ -13,10 +13,35 @@ import org.springframework.stereotype.Service;
 
 
 
-    @Service
-    public class GreetingService {
-        public String getGreetingMessage() {
-            return "Hello World";
-        }
+//    @Service
+//    public class GreetingService {
+//        public String getGreetingMessage() {
+//            return "Hello World";
+//        }
+
+import org.springframework.stereotype.Service;
+
+
+import com.greetingapp.repository.GreetingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingService {
+
+    private final GreetingRepository greetingRepository;
+
+    @Autowired
+    public GreetingService(GreetingRepository greetingRepository) {
+        this.greetingRepository = greetingRepository;
     }
+
+    public String getGreetingMessage(String firstName, String lastName) {
+        return greetingRepository.getGreetingMessage(firstName, lastName);
+    }
+}
+
+
+
+
 
