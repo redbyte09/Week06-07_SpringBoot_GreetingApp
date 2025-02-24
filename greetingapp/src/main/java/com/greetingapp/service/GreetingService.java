4e4a8571aa0ed88c8f894aa2d7099352ed77204c
redbyte09 <sakshisharma4234@gmail.com>
2025-02-24 11:46:10 +0530

    [Sakshi] Add . Ability for the Greeting App to find a message

--- greetingapp/src/main/java/com/greetingapp/service/GreetingService.java
+++ greetingapp/src/main/java/com/greetingapp/service/GreetingService.java
@@ -1,12 +1,17 @@
-
 package com.greetingapp.service;
+
 
 import com.greetingapp.model.Greeting;
 import com.greetingapp.repository.GreetingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
 @Service
 public class GreetingService {
+
+
     private final GreetingRepository greetingRepository;
 
     @Autowired
@@ -14,20 +19,27 @@ public class GreetingService {
         this.greetingRepository = greetingRepository;
     }
 
-   /* public String getGreetingMessage() {
-            return "Hello World";
-        }
-   /* public String getGreetingMessage(String firstName, String lastName) {
-        if (firstName != null && lastName != null) {
-            return "Hello " + firstName + " " + lastName;
-        } else if (firstName != null) {
-            return "Hello " + firstName;
-        } else if (lastName != null) {
-            return "Hello " + lastName;
+
+    /* UC-01
+    public String getGreetingMessage() {
+        return "Hello World";
+    }
+    */
+
+    /*UC-02
+    public String getGreetingMessage(String firstName, String lastName) {
+        if (firstName != null && !firstName.isEmpty() && lastName != null && !lastName.isEmpty()) {
+            return "Hello, " + firstName + " " + lastName + "!";
+        } else if (firstName != null && !firstName.isEmpty()) {
+            return "Hello, " + firstName + "!";
+        } else if (lastName != null && !lastName.isEmpty()) {
+            return "Hello, " + lastName + "!";
         } else {
             return "Hello World";
-     }
-  }*/
+        }
+    }
+    */
+
 
     //UC-04
     public String getGreetingMessage(String firstName, String lastName) {
@@ -47,6 +59,13 @@ public class GreetingService {
         greetingRepository.save(greeting);
 
         return message;
+    }
+
+
+    //UC-05
+    // New method to find a greeting by ID
+    public Optional<Greeting> getGreetingById(Long id) {
+        return greetingRepository.findById(id);
     }
 
 }
